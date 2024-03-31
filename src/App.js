ec845a96743bcfdb15ecbbbdc73a8710fe908ce5
MaazPro <maazmuhammad94@gmail.com>
2024-03-31 18:59:41 +0800

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import DataTable from "react-data-table-component";
 
+const customStyles = {
+  headRow: {
+    style: {
+      backgroundColor: "blue",
+      color: "white",
+    },
+  },
+  headCells: {
+    style: {
+      fontSize: "16px",
+      fontWeight: "600",
+      textTransform: "uppercase",
+    },
+  },
+  cells: {
+    style: {
+      fontSize: "15px",
+    },
+  },
+};
 function App() {
+  const column = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+      sortable: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+    },
+    {
+      name: "City",
+      selector: (row) => row.address.city,
+    },
+  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+          setRecords(res.data);
+          setFilterRecords(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+    fetchData();
+  }, []);
+  const [records, setRecords] = useState([]);
+  const [filterRecords, setFilterRecords] = useState([]);
+
+  const handleFilter = (event) => {
+    const newData = filterRecords.filter((row) =>
+      row.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setRecords(newData);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "50px 10%", backgroundColor: "gray" }}>
+      <div style={{ display: "flex", justifyContent: "right" }}>
+        <input
+          type="text"
+          placeholder="Search..."
+          style={{ padding: "6px 10px" }}
+          onChange={handleFilter}
+        />
+      </div>
+      <DataTable
+        columns={column}
+        data={records}
+        customStyles={customStyles}
+        pagination
+        selectableRows
+      ></DataTable>
     </div>
   );
 }
